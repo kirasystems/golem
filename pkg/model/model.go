10cd2218cb8570f782ec4e3c7c61ff4f3e6a7150
matteo-grella <matteogrella@gmail.com>
2020-12-09 00:30:06 +0100

    Add missing pointer; raise questions

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -2,10 +2,11 @@ package model
 
 import "github.com/nlpodyssey/spago/pkg/ml/nn"
 
-type CategoricalFeatureEmbedding map[string]nn.Param
+type CategoricalFeatureEmbedding map[string]*nn.Param
 
 type Model struct {
-	MetaData                     *Metadata
-	TabNet                       *TabNet
-	CategoricalFeatureEmbeddings map[int]CategoricalFeatureEmbedding
+	MetaData *Metadata
+	TabNet   *TabNet
+	// @jjviana why do we need a two level map? Isn't enough a simple map[string]*nn.Param ?
+	CategoricalFeatureEmbeddings map[int]CategoricalFeatureEmbedding // TODO: check if spaGO supports this
 }
